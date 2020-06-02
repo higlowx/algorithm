05ecf5c787184bae9f556f866c454c0a54eafe7e
李希浩 <lixihaofree@163.com>
2020-06-02 17:37:23 +0800

    commit letcoode HammingWeight.java

---
+++ src/FindClosedNumbers.java
@@ -0,0 +1,26 @@
+/**
+ * TODO
+ * 下一个数。给定一个正整数，找出与其二进制表达式中1的个数相同且大小最接近的那两个数（一个略大，一个略小）。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入：num = 2（或者0b10）
+ * 输出：[4, 1] 或者（[0b100, 0b1]）
+ * 示例2:
+ * <p>
+ * 输入：num = 1
+ * 输出：[2, -1]
+ * 提示:
+ * <p>
+ * num的范围在[1, 2147483647]之间；
+ * 如果找不到前一个或者后一个满足条件的正数，那么输出 -1。
+ * <p>
+ * https://leetcode-cn.com/problems/closed-number-lcci
+ *
+ * @author Dylan.Lee
+ * @date 2020/6/2
+ * @since
+ */
+
+public class FindClosedNumbers {
+}
---
+++ src/SingleNumber2.java
@@ -0,0 +1,15 @@
+/**
+ * TODO
+ * @author Chris.Li
+ * @desc
+ * @date 2020/5/20
+ */
+
+public class SingleNumber2 {
+//
+//    public int singleNumber(int[] nums) {
+//        for (int num : nums) {
+//
+//        }
+//    }
+}
