8d2878841231f97c320ca5b531ed5f8c1f4ec885
李希浩 <lixihaofree@163.com>
2020-12-17 19:00:36 +0800

    todo JzOffer35;

--- src/main/java/com/higlowx/algorithm/jzoffer/JzOffer35.java
+++ src/main/java/com/higlowx/algorithm/jzoffer/JzOffer35.java
@@ -18,11 +18,12 @@ public class JzOffer35 {
 
     /**
      * 解法一：暴力双层循环
+     * 有执行超时风险
      * <p>
      * 时间复杂度：O(N^2)
      * 空间复杂度：O(1)
      */
-    public int reversePairs(int[] nums) {
+    public int reversePairs0(int[] nums) {
         int result = 0;
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -31,4 +32,47 @@ public class JzOffer35 {
         }
         return result;
     }
+
+    /**
+     * 方法二：归并排序
+     * <p>
+     * 概念讲解：归并排序
+     * 给定两个顺序数组 array1{1,3,6,8}与array2{0,2,4,9}，将这两个有序数组合并为一个顺序递增数组
+     * 声明两个指针i，j，分别指向两个数组的第一个位置，声明一个中间数组temp，用于存储合并后的数组
+     * 比较 array1[i] 与 array[j] 的大小关系，如果前者小于后者，则向temp追加前者，i后移一个单位，反之，则追加后者，j后移一个单位
+     * 循环往复，直至其中某个数组（或两个都）被遍历完毕，将剩下的未遍历完毕的数组整个追加到temp中，然后结束合并；如果都没剩下，则直接结束合并；
+     * 这样temp数组就是我们最终想要得到的顺序递增数组
+     * <p>
+     * 归并排序的几个核心点在于：
+     * 1、合并前的两个数组一定都得是 顺序 的
+     * 2、需要中间数组进行合并集的存储
+     * 3、需要双指针
+     * <p>
+     * Q：那么在该题目中，如何使用归并排序来统计数组中逆序对的数量呢？
+     * A：利用归并排序 对合并前 两个数组的 顺序 特性，减少逐一比较的资源、时间消耗，在排序的同时，进行逆序对的统计
+     */
+    public int reversePairs1(int[] nums) {
+        int[] temp = new int[nums.length];
+
+        return 0;
+    }
+
+    private int reversePairs1(int[] nums, int left, int right, int[] temp) {
+        if (left == right) {
+            return 0;
+        }
+        int mid = left + (right - left) / 2;
+        int leftCount = reversePairs1(nums, left, mid, temp);
+        int rightCount = reversePairs1(nums, mid + 1, right, temp);
+        int mergeCount = mergeAndCount(nums, left, mid, right, temp);
+        return leftCount + rightCount + mergeCount;
+    }
+
+    private int mergeAndCount(int[] nums, int left, int mid, int right, int[] temp) {
+
+
+        return 0;
+    }
+
+
 }
