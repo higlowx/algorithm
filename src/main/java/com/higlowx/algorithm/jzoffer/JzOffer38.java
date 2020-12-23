15c5addea377c9c72625a7f39fbd6510ca3e2d20
李希浩 <lixihaofree@163.com>
2020-12-23 10:40:31 +0800

    commit JzOffer38;

--- src/main/java/com/higlowx/algorithm/jzoffer/JzOffer38.java
+++ src/main/java/com/higlowx/algorithm/jzoffer/JzOffer38.java
@@ -1,7 +1,5 @@
 package com.higlowx.algorithm.jzoffer;
 
-import java.util.Stack;
-
 /**
  * 二叉树的深度
  * <p>
@@ -20,7 +18,6 @@ import java.util.Stack;
  * 提示：
  * 节点总数 <= 10000
  * <p>
- * 来源：力扣（LeetCode）
  * leetcode：https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof
  * nowcoder：https://www.nowcoder.com/practice/435fb86331474282a3499955f0a41e8b
  *
@@ -32,37 +29,23 @@ public class JzOffer38 {
 
 
     /**
-     * 解法一：深度优先搜索（DFS）
-     * <p>
-     * 参考 二叉树的层级遍历 中使用BFS并引入 辅助队列 的方式
-     * 这里，我们将使用 辅助栈 这种数据结构 来实现
-     * 栈的特性是 后进先出
+     * 解法一：递归
+     * 时间复杂度:O(N)，每个节点都要递归一次，递归N次，每次递归的时间复杂度是O(1)，所以最终为O(N)
+     * 空间复杂度:递归栈空间
      */
     public int treeDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int depth = 1;
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        stack.push(root);
-        int tempDepth = 1;
-        while (!stack.isEmpty()) {
-            TreeNode peek = stack.peek();
-            //当栈顶元素是root时，表明前一次深度优先搜索已经结束
-            //这是需要计算得出目前的最大深度，并存储至depth中，tempDepth重新置为1
-            if (peek == root) {
-                depth = tempDepth > depth ? tempDepth : depth;
-                tempDepth = 1;
-            }
-            tempDepth += 1;
-            if (peek.left != null) {
-                stack.push(peek.left);
-            }
-            if (peek.right != null) {
-                stack.push(peek.right);
-            }
-        }
-        return 0;
+        return root == null ? 0 : Math.max(treeDepth(root.left), treeDepth(root.right)) + 1;
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.left.left = new TreeNode(15);
+        JzOffer38 ins = new JzOffer38();
+        int depth = ins.treeDepth(root);
+        System.out.println(depth);
     }
 
     private static class TreeNode {
